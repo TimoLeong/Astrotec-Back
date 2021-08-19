8a480b7f49fbfb180cce323893f3a71346fef26b
TimoLeong <timothy.ivan.leong@gmail.com>
2021-08-19 16:10:52 +0200

    Fin création membre

--- Astrotec-Back/src/main/java/fr/solutec/entities/Membre.java
+++ Astrotec-Back/src/main/java/fr/solutec/entities/Membre.java
@@ -1,5 +1,37 @@
 package fr.solutec.entities;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.beans.factory.annotation.Value;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor @Data
 public class Membre {
-	/*yo*/
+	
+	@Id @GeneratedValue
+	private Long id;
+	@NonNull private String email;
+	@NonNull  String pseudo;
+	@NonNull private String mdp;
+	private String nom;
+	private String prenom;
+	private Date ddn;
+	
+	@CreationTimestamp
+	private Timestamp dateCreation;
+	
+	@Value("${admin:false}")
+	private boolean admin;
+	
 }
