dce702005cc480a092b25d13a1d4f58f8dc96872
TimoLeong <timothy.ivan.leong@gmail.com>
2021-08-19 13:59:55 +0200

    First Commit

---
+++ Astrotec-Back/src/main/java/fr/solutec/AstrotecBackApplication.java
@@ -0,0 +1,13 @@
+package fr.solutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrotecBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrotecBackApplication.class, args);
+	}
+
+}
