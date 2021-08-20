2d8101ba2f40270c4d36d92bca88fa8f1592e55e
Qqefswza1/ <jbrx17@gmail.com>
2021-08-20 10:25:34 +0200

    add attributs

--- Astrotec-Back/src/main/java/fr/solutec/entities/Planete.java
+++ Astrotec-Back/src/main/java/fr/solutec/entities/Planete.java
@@ -14,7 +14,16 @@ public class Planete {
 	@Id@GeneratedValue
 	private Long id;
 	
-	private String nom;
-	private String masse;
-	private String distance;
+	private String name;
+	private Long diameter;
+	private Long mass;
+	private Long gravity;
+	private Long length_of_day;
+	private Long distance_from_sun;
+	private Long orbital_period;
+	private	Long mean_temperature;
+	private Long number_of_moons;
+	private Boolean has_ring_system;
+	private Boolean has_global_magnetic_field;
+	
 }
