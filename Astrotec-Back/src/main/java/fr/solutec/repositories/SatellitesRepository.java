1c9b4007dbbe8dfc914c38f3c7a315d21426d959
Qqefswza1/ <jbrx17@gmail.com>
2021-08-20 13:55:57 +0200

    remove files

--- Astrotec-Back/src/main/java/fr/solutec/repositories/SatellitesRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.solutec.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fr.solutec.entities.Satellites;
-
-public interface SatellitesRepository extends CrudRepository<Satellites, Long>{
-
-}
