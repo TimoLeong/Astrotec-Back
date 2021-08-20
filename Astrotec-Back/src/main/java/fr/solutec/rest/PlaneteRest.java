03adcc5b91cc6a79c6fa09498f2760e48f78a8d0
Qqefswza1/ <jbrx17@gmail.com>
2021-08-20 10:24:20 +0200

    add getAllPanete getPlanete

--- Astrotec-Back/src/main/java/fr/solutec/rest/PlaneteRest.java
+++ Astrotec-Back/src/main/java/fr/solutec/rest/PlaneteRest.java
@@ -1,14 +1,29 @@
 package fr.solutec.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.entities.Planete;
 import fr.solutec.repositories.PlaneteRepository;
 
-@RestController @CrossOrigin("*")
+@RestController
+@CrossOrigin("*")
 public class PlaneteRest {
 	@Autowired
 	private PlaneteRepository planeteRepo;
-	
+
+	@GetMapping("planete")
+	public Iterable<Planete> getAllPanete() {
+		return planeteRepo.findAll();
+	}
+
+	@GetMapping("planete/{id}")
+	public Optional<Planete> getPlanete(@PathVariable Long id) {
+		return planeteRepo.findById(id);
+	}
 }
