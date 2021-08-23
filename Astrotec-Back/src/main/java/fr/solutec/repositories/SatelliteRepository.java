9bb4d22deb7d64118e1fb3cf3edec7a21021cb5e
jb <jbrx17@gmail.com>
2021-08-23 10:27:26 +0200

    change Long to Double

--- Astrotec-Back/src/main/java/fr/solutec/repositories/SatelliteRepository.java
+++ Astrotec-Back/src/main/java/fr/solutec/repositories/SatelliteRepository.java
@@ -10,4 +10,4 @@ public interface SatelliteRepository extends CrudRepository<Satellite, Long>{
 	
 	public List<Satellite> findByplanetid(Long id);
 	public List<Satellite> findByName(String name);
-}
+} 
