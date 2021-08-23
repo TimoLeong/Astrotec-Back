cf1f79fd9332f2e577a89957087417caff20b25c
Quentin Lecroart <qnkapplication@gmail.com>
2021-08-23 13:36:52 +0200

    prod update

--- Astrotec-Back/src/main/java/fr/solutec/rest/ProduitRest.java
+++ Astrotec-Back/src/main/java/fr/solutec/rest/ProduitRest.java
@@ -1,8 +1,11 @@
 package fr.solutec.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Produit;
@@ -18,5 +21,10 @@ public class ProduitRest {
 	@GetMapping("produit")
 	public Iterable<Produit> getAllProduit() {
 		return produitRepository.findAll();
+	}
+	
+	@GetMapping("produit/{id}")
+	public Optional<Produit> getProduit(@PathVariable Long id) {
+		return produitRepository.findById(id);
 	}
 }
