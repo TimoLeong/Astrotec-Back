d90d2e356d68852937d44e152ab029a2eb7257b5
Quentin Lecroart <qnkapplication@gmail.com>
2021-08-23 11:59:05 +0200

    get produit

--- Astrotec-Back/src/main/java/fr/solutec/rest/ProduitRest.java
+++ Astrotec-Back/src/main/java/fr/solutec/rest/ProduitRest.java
@@ -2,8 +2,10 @@ package fr.solutec.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.entities.Produit;
 import fr.solutec.repositories.ProduitRepository;
 
 @RestController
@@ -12,4 +14,9 @@ public class ProduitRest {
 	
 	@Autowired
 	private ProduitRepository produitRepository;
+	
+	@GetMapping("produit")
+	public Iterable<Produit> getAllPanete() {
+		return produitRepository.findAll();
+	}
 }
