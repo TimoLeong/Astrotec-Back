b85f872aee79efec034153fafc026d9357e5b4da
Quentin Lecroart <qnkapplication@gmail.com>
2021-08-26 17:54:21 +0200

    prod

--- Astrotec-Back/src/main/java/fr/solutec/AstrotecBackApplication.java
+++ Astrotec-Back/src/main/java/fr/solutec/AstrotecBackApplication.java
@@ -1,12 +1,17 @@
 package fr.solutec;
 
+
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class AstrotecBackApplication implements CommandLineRunner{
-	
+		
+		
 	public static void main(String[] args) {
 		SpringApplication.run(AstrotecBackApplication.class, args);
 		System.out.println("Lancement Terminé");
@@ -15,6 +20,8 @@ public class AstrotecBackApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Début Instruction");
+
+		
 	}
 
 }
--- Astrotec-Back/src/main/java/fr/solutec/entities/Produit.java
+++ Astrotec-Back/src/main/java/fr/solutec/entities/Produit.java
@@ -3,6 +3,7 @@ package fr.solutec.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class Produit {
 	@Id @GeneratedValue
 	private Long id;
+	
 	private String nom;
 	private String description;
 	private String image;
